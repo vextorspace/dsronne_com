09b55916d1743f2de76aaef6104d6848bae72efc
Douglas Ronne <vextorspace@gmail.com>
2025-04-05 11:05:00 -0700

    working navajo page

--- dsronne_com/dsronne_com/settings.py
+++ dsronne_com/dsronne_com/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [ BASE_DIR / 'dsronne_com' / 'static',]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
